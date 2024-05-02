1fa3ccdb4be68b2a8f33a7cf1a48f5c416767f37
Laurel (they/he) <laurel.hiatt@gmail.com>
2024-05-02 17:24:30 -0600

    update x label

--- scripts/Figure4.R
+++ scripts/Figure4.R
@@ -9,8 +9,6 @@
 #   coord_flip() +
 #   scale_y_continuous(sec.axis = dup_axis())
 
-
-
 # for CNG only
 # ggplot(data = filter(combined_df, combined_df$Motif == "CNG"), aes(x = reorder(gene, pathogenic_percent), y = pathogenic_percent)) +
 #   geom_errorbar(aes(ymin = Pathogenic_Count_lower_ci, ymax = Pathogenic_Count_upper_ci), width = 0.3, size = 2, color = "black") +
@@ -27,26 +25,23 @@ ggplot(data = filter(combined_df, !is.na(combined_df$prevalence)), aes(x = reord
   geom_errorbar(aes(ymin = Pathogenic_Count_lower_ci, ymax = Pathogenic_Count_upper_ci), width = 0.3, size = 2, color = "black") +
   geom_point(aes(y = prevalence_dec*100), fill = "#E66100", size = 5, alpha = 0.8, shape = 23) +
   geom_point(aes(y = pathogenic_percent), color = "#5D3A9B", size = 7, alpha = 0.9, shape = 20) +
-  labs(x = "Gene", y = "Percentage") +
+  labs(x = "Gene", y = "Percent of individuals") +
   theme(axis.text.x = element_text(vjust = 0.5, hjust=1, size =15),
         plot.title = element_text(hjust = 0.5)) +
   coord_flip() +
   theme(axis.text.y = element_text(colour = color_labels)) +
-  scale_y_continuous(sec.axis = dup_axis()) +
   theme_minimal()
-
 
 
 ggplot(data = filter(combined_df, !is.na(combined_df$prevalence)), aes(x = reorder(gene, pathogenic_percent), y = pathogenic_percent)) +
   geom_errorbar(aes(ymin = Pathogenic_Count_lower_ci, ymax = Pathogenic_Count_upper_ci), width = 0.3, size = 2, color = "black") +
   geom_point(aes(y = prevalence_dec*100), fill = "#E66100", size = 5, alpha = 0.8, shape = 23) +
   geom_point(aes(y = pathogenic_percent), color = "#5D3A9B", size = 7, alpha = 0.9, shape = 20) +
-  labs(x = "Gene", y = "Percentage") +
+  labs(x = "Gene", y = "Percent of individuals") +
   theme(axis.text.x = element_text(vjust = 0.5, hjust=1, size =15),
         plot.title = element_text(hjust = 0.5)) +
   coord_flip() +
   theme(axis.text.y = element_text(colour = color_labels)) +
-  scale_y_continuous(sec.axis = dup_axis()) +
   ylim(0,0.64) +
   theme(axis.text.y = element_text(colour = color_labels)) +
   theme_minimal()
@@ -58,13 +53,14 @@ ggplot(data = filter(combined_df, !is.na(combined_df$prevalence)), aes(x = reord
 # ggplot(data = filter(combined_df, !is.na(combined_df$prevalence)),
 #        aes(x = reorder(gene, pathogenic_percent), y = pathogenic_percent)) +
 #   geom_errorbar(aes(ymin = Pathogenic_Count_lower_ci, ymax = Pathogenic_Count_upper_ci), width = 0.5, size = 3, color = "black") +
-#   geom_point(aes(y = prevalence_dec*100), fill = "blue", size = 15, alpha = 0.8, shape = 23) +
-#   geom_point(aes(y = pathogenic_percent), color = "red", size = 30, alpha = 0.9, shape = 20) +
+#   geom_point(aes(y = prevalence_dec*100), fill = "#E66100", size = 16, alpha = 0.8, shape = 23) +
+#   geom_point(aes(y = pathogenic_percent), color = "#5D3A9B", size = 27, alpha = 0.9, shape = 20) +
 #   labs(x = "Gene", y = "Percentage") +
 #   coord_flip() +
 #   theme(axis.text.x = element_text(vjust = 0.5, hjust=1, size =15),
 #         plot.title = element_text(hjust = 0.5)) +
 #   ylim(0,0.64) +
-#   theme(axis.text.y = element_text(colour = color_labels))
-#
+#   theme(axis.text.y = element_text(colour = color_labels)) +
+#   theme_void()
+
 
